25947f278f29d3db78d737ccca9a68bc027dc8ef
Yojijuku5 <55696714+Yojijuku5@users.noreply.github.com>
2024-01-07 01:19:44 +0000

    Update main.py

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 #automated using task scheduler
 from misskey import Misskey;
 
-instance = Misskey("misskey.io", i="sXqmDUjXOgGG2v8iilGNUASWK6V21CIj")
+instance = Misskey("misskey.io", i="REPLACE WITH PERSONAL MISSKEY ACCESS TOKEN")
 
 
 #post yayo note
@@ -10,5 +10,5 @@ new_note = instance.notes_create(file_ids=yayo_Id)
 print(new_note["createdNote"]["id"]) #print note id
 
 #file information - used to get file id
-#getFile = instance.drive_files_show(url="https://media.misskeyusercontent.com/io/27b8d21f-30f6-4882-8616-f3e534ed8251.jpg")
+#getFile = instance.drive_files_show(url="IN MISSKEY, GO TO 'DRIVE', AND FIND FILE TO BE USED")
 #print(getFile)
